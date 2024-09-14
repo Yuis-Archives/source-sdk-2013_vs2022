fdb763f726b161e94370bb73e48963ce01bc0289
Yui <50331474+SirYodaJedi@users.noreply.github.com>
2024-09-13 21:43:15 -0400

    ybsp install script to auto-copy to game bin folders
    Replaces the old dummy.txt

---
+++ mp/game/bin/ybsp_install.bat
@@ -0,0 +1,9 @@
+REM This is mostly here for my own benefit, but you can use it too if you set up the appropriate environment variables.
+REM It also prevents Git from ignoring this folder, so that Visual Studio copies the compiled EXE here correctly.
+copy ybsp.exe %css%\bin
+copy ybsp.exe %dods%\bin
+copy ybsp.exe %hl2dm%\bin
+copy ybsp.exe %hldms%\bin
+copy ybsp.exe %portal%\bin
+copy ybsp.exe %src13mp%\bin
+copy ybsp.exe %tf2%\bin
